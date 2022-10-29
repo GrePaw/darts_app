a12f26647f9ef94e95c2859698f500c9059d8c18
GrePaw <grepaw@outlook.de>
2022-10-29 12:57:48 +0200

    Style and layout adjusted with linux machine

--- darts_app/karma.conf.js
+++ darts_app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/darts_app'),
+      dir: require('path').join(__dirname, './coverage/exampleapplication'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- darts_app/src/app/app.component.ts
+++ darts_app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'dapp-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'darts_app';
+  title = 'exampleapplication';
 }
